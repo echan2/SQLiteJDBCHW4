00cce02e0cfaa4eb6906b73c93abd5060466e91d
Ellison Chan <omnifas2@gmail.com>
2016-05-15 12:07:48 -0700

    first

---
+++ src/sqlitejdbchw4/SQLiteJDBCHW4.java
@@ -0,0 +1,23 @@
+package sqlitejdbchw4;
+
+/**
+ * @description SQLite JDBC Application of Chinok.db
+ * @author Ellison Chan
+ * @SFSUID 912262733
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteJDBCHW4
+{
+
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
